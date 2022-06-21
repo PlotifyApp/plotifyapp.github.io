1f2c6aae5b62d477bca873361a7b9858373df994
kailyl <kliu279@gmail.com>
2022-06-20 21:51:50 -0400

    Updates

---
+++ precache-manifest.574429237360ac07b5ebedf52fb8c519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6708b703b85d7ab73c8b",
+    "url": "/static/css/main.dd2283cf.chunk.css"
+  },
+  {
+    "revision": "6708b703b85d7ab73c8b",
+    "url": "/static/js/main.149c8cde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "575a8dc04a815fabee03",
+    "url": "/static/js/2.10af9504.chunk.js"
+  },
+  {
+    "revision": "4791f781dc2611c7de578f706cabec6e",
+    "url": "/static/media/testLogo.4791f781.png"
+  },
+  {
+    "revision": "d0f0a336c6fb85a2beda032079733b60",
+    "url": "/index.html"
+  }
+];
